be27fb464bfb48dbe65d9940370a3ce2000eaa80
ashankaushalya97 <ashankaushalya952@gmail.com>
2019-12-11 16:54:43 +0530

    initial commit

---
+++ dist/js/database.js
@@ -0,0 +1,12 @@
+var customers = [
+    {
+        id:"C001",
+        name:"Alisa",
+        address:"Pinwatta"
+    },
+    {
+        id:"C002",
+        name:"Leel",
+        address:"Mathugama"
+    }
+];
